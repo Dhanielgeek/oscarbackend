ac6af7d847b908765d1975c347a6e424a2e72e4c
kerry esua <kerryesua9@gmail.com>
2025-02-06 23:08:53 +0100

    fixing 2

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const cors = require("cors")
 const errorHandler = require("./middlewares/errorHandler")
 const blogRoute = require("./routes/blogRoute")
 const imageRoute = require("./routes/imageRoute")
@@ -7,6 +8,17 @@ const app = express()
 require("dotenv").config()
 
 const db = require("./models")
+
+const corsOptions = {
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+
+    ],
+    optionalSuccessStatus: 200,
+  };
+  
+  app.use(cors(corsOptions));
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use("/api", blogRoute)
