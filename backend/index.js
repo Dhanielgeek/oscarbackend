6f166275fd97591b5a8c6400826f57f86cce8e6c
kerry esua <kerryesua9@gmail.com>
2025-02-12 17:57:07 +0100

    fix 13

--- backend/index.js
+++ backend/index.js
@@ -25,15 +25,15 @@ const db = require("./models")
   
   //app.use(cors(corsOptions));
 
-  app.use((req,res,next)=>{
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT,DELETE,OPTIONS")
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
-    if (req.method==="OPTIONS") {
-        return res.status(200)
-    }
-    next()
-  })
+//   app.use((req,res,next)=>{
+//     res.header("Access-Control-Allow-Origin", "*")
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT,DELETE,OPTIONS")
+//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+//     if (req.method==="OPTIONS") {
+//         return res.status(200)
+//     }
+//     next()
+//   })
 app.use(cors({
     origin: "*",
     credentials: true,
