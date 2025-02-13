dd089e3e9d9196ffb21738aac1bc0debb6c2f674
Dhanielgeek <dhanielknightz46@gmail.com>
2025-02-13 10:06:36 +0100

    Testing the new origin isseue for cors..

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
-const express = require("express")
-const cors = require("cors")
-require("dotenv").config()
-const errorHandler = require("./middlewares/errorHandler")
-const blogRoute = require("./routes/blogRoute")
-const imageRoute = require("./routes/imageRoute")
-const adminRoute = require("./routes/adminRoute")
-const app = express()
-const db = require("./models")
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const errorHandler = require("./middlewares/errorHandler");
+const blogRoute = require("./routes/blogRoute");
+const imageRoute = require("./routes/imageRoute");
+const adminRoute = require("./routes/adminRoute");
+const app = express();
+const db = require("./models");
 
 /*const corsOptions = {
     origin:(origin,callback)=>{
@@ -20,32 +20,33 @@ const db = require("./models")
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET","POST","PUT","DELETE","OPTIONS"]
   };*/
-  
-  //app.use(cors(corsOptions));
 
-  app.use((req,res,next)=>{
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT,DELETE,OPTIONS")
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
-    if (req.method==="OPTIONS") {
-        return res.status(200)
-    }
-    next()
-  })
-   //app.use(cors())
-   app.use((req,res,next)=>{
-    console.log(res.getHeaders() || "none")
-    next()
-  })
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
-app.use("/api", blogRoute)
-app.use("/api", imageRoute)
-app.use("/api", adminRoute)
-app.use(errorHandler)
+const corsOption = ["http://localhost:5173"];
+app.use(cors(corsOption));
 
-const port = process.env.PORT
-db.sequelize.sync({alter:true}).then(()=>{})
-app.listen(process.env.PORT, ()=>{
-    console.log(`server running on port ${port}`)
-})
+//   app.use((req,res,next)=>{
+//     res.header("Access-Control-Allow-Origin", "*")
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT,DELETE,OPTIONS")
+//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+//     if (req.method==="OPTIONS") {
+//         return res.status(200)
+//     }
+//     next()
+//   })
+//    //app.use(cors())
+//    app.use((req,res,next)=>{
+//     console.log(res.getHeaders() || "none")
+//     next()
+//   })
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/api", blogRoute);
+app.use("/api", imageRoute);
+app.use("/api", adminRoute);
+app.use(errorHandler);
+
+const port = process.env.PORT;
+db.sequelize.sync({ alter: true }).then(() => {});
+app.listen(process.env.PORT, () => {
+  console.log(`server running on port ${port}`);
+});
